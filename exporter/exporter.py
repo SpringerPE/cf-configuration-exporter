c00a2c9555ce30c816a79c5415d22a03fa7c84e8
Claudio Benfatto <claudio.benfatto@gmail.com>
2017-02-15 17:24:25 +0000

    Introduce BaseEntity base class

--- exporter/exporter.py
+++ exporter/exporter.py
@@ -5,15 +5,6 @@ import yaml
 import pyaml
 import os
 
-class GUIDHelper:
-
-    def __init__(self, config):
-        self._config = config
-
-    def get_guids(self):
-        guids = [resource['metadata']['guid'] for resource in self._config[0]['resources']]
-        return guid
-
 class ResourceFetcher:
 
     def __init__(self, client):
@@ -31,7 +22,29 @@ class ResourceFetcher:
         response = self._client.request("GET", resource_url)
         return [obj['metadata'] for obj in response[0]['resources']]
 
-class Quota:
+class BaseEntity:
+
+    def __init__(self, config):
+        self._prop_dict = collections.OrderedDict()
+        self._config = config
+
+    def __getattr__(self, name):
+        if name in self._config:
+            return self._config[name]
+        raise AttributeError("%s not found" % name)
+
+    def load(self):
+        for prop in self.properties:
+            try:
+                value = getattr(self, prop)
+                self._prop_dict[prop] = value
+            except AttributeError as ate:
+                pass
+
+    def asdict(self):
+        return self._prop_dict
+
+class Quota(BaseEntity):
 
     properties = [  "name", 
                             "total_services", 
@@ -46,33 +59,21 @@ class Quota:
                         ]
 
     def __init__(self, config):
-        self._prop_dict = collections.OrderedDict()
-        self._config = config
-        self._quotas = []
+        super(Quota, self).__init__(config)
 
-    def load(self):
-        for prop in self.properties:
-            try:
-                value = getattr(self, prop)
-                self._prop_dict[prop] = value
-            except AttributeError as ate:
-                pass
+class Space(BaseEntity):
 
-    def asdict(self):
-        return self._prop_dict
-
-    def __getattr__(self, name):
-        if name in self._config:
-            return self._config[name]
-        raise AttributeError("%s not found" % name)
-
-class Space:
-
-    properties = ["name", "allow_ssh", "developers", "managers", "auditors", "security_groups"]
+    properties = [
+                            "name", 
+                            "allow_ssh", 
+                            "developers", 
+                            "managers", 
+                            "auditors", 
+                            "security_groups"
+                        ]
     
     def __init__(self, config, fetcher):
-        self._prop_dict = collections.OrderedDict()
-        self._config = config
+        super(Space, self).__init__(config)
         self._fetcher = fetcher
         self._security_groups = []
 
@@ -102,28 +103,22 @@ class Space:
     def load(self):
         self.load_users()
         self.load_security_groups()
-        for prop in self.properties:
-            try:
-                value = getattr(self, prop)
-                self._prop_dict[prop] = value
-            except AttributeError as ate:
-                pass
+        BaseEntity.load(self)
 
-    def asdict(self):
-        return self._prop_dict
 
-    def __getattr__(self, name):
-        if name in self._config:
-            return self._config[name]
-        raise AttributeError("%s not found" % name)
+class Organization(BaseEntity):
 
-class Organization:
-
-    properties = ["name", "spaces", "users", "managers", "billing_managers", "auditors"]
+    properties = [
+                            "name", 
+                            "spaces", 
+                            "users", 
+                            "managers", 
+                            "billing_managers", 
+                            "auditors"
+                        ]
 
     def __init__(self, config, fetcher):
-        self._prop_dict = collections.OrderedDict()
-        self._config = config
+        super(Organization, self).__init__(config)
         self._fetcher = fetcher
         self._spaces = []
 
@@ -155,127 +150,90 @@ class Organization:
     def load(self):
         self.load_spaces()
         self.load_users()
-        for prop in self.properties:
-            try:
-                value = getattr(self, prop)
-                self._prop_dict[prop] = value
-            except AttributeError as ate:
-                pass
+        BaseEntity.load(self)
 
-    def asdict(self):
-        return self._prop_dict
+class SecurityGroup(BaseEntity):
 
-    def __getattr__(self, name):
-        if name in self._config:
-            return self._config[name]
-        raise AttributeError("%s not found" % name)
-
-class SecurityGroup:
-
-    properties = ["name", "context", "rules"]
+    properties = [
+                            "name", 
+                            "context", 
+                            "rules"
+                        ]
 
     def __init__(self, config):
-        self._prop_dict = collections.OrderedDict()
-        self._config = config
+        super(SecurityGroup, self).__init__(config)
         self._rules = []
-
-    def load(self):
-        rules = self._config['rules']
-        for rule in rules:
-            self.add_rule(rule)
-        for prop in self.properties:
-            try:
-                value = getattr(self, prop)
-                self._prop_dict[str(prop)] = value
-            except AttributeError:
-                pass
-
-    def asdict(self):
-        return self._prop_dict
 
     @property
     def rules(self):
         return self._rules
 
-    def add_rule(self, rule):
-        new_rule = SecurityRule(rule)
-        new_rule.load()
-        self._rules.append(new_rule.asdict())
+    def load(self):
+        self.load_rules()
+        BaseEntity.load(self)
 
-    def __getattr__(self, name):
-        if name in self._config:
-            return self._config[name]
-        raise AttributeError("%s not found" % name)
+    def load_rules(self):
+        rules = self._config['rules']
+        for rule in rules:
+            new_rule = SecurityRule(rule)
+            new_rule.load()
+            self._rules.append(new_rule.asdict())
 
-class SecurityRule:
+class SecurityRule(BaseEntity):
 
-    properties = ["name", "protocol", "destination", "ports", "logs", "code", "type"]
+    properties = [
+                            "name", 
+                            "protocol", 
+                            "destination", 
+                            "ports", 
+                            "logs", 
+                            "code", 
+                            "type"
+                        ]
 
     def __init__(self, config):
-        self._config = config
-        self._prop_dict = collections.OrderedDict()
-
-    def asdict(self):
-        return self._prop_dict
-
-    def load(self):
-        for prop in self.properties:
-            try:
-                value = getattr(self, prop)
-                self._prop_dict[prop] = value
-            except AttributeError:
-                pass
-
-    def __getattr__(self, name):
-        if name in self._config:
-            return self._config[name]
-        raise AttributeError("%s not found" % name)
+        super(SecurityRule, self).__init__(config)
 
 
-class User:
+class User(BaseEntity):
 
-    properties = ["name", "active", "email", "given_name", "family_name",
-                  "default_organization", "default_space", "origin", "external_id"]
+    properties = [
+                            "name", 
+                            "active", 
+                            "email", 
+                            "given_name", 
+                            "family_name",
+                            "default_organization", 
+                            "default_space", 
+                            "origin", 
+                            "external_id"
+                        ]
 
-    def __init__(self, user_cf, user_uaa):
-        self._user_cf = user_cf
+    def __init__(self, config, user_uaa):
+        super(User, self).__init__(config)
         self._user_uaa = user_uaa
-        self._prop_dict = collections.OrderedDict()
-
-    def asdict(self):
-        return self._prop_dict
 
     @property
     def given_name(self):
-        response = None
         name = self.__getattr__('name')
         if 'givenName' in name:
-            response = name['givenName']
-        return response
+            return name['givenName']
+        raise AttributeError("given_name not found")
 
     @property
     def family_name(self):
-        response = None
         name = self.__getattr__('name')
         if 'familyName' in name:
-            response = name['familyName']
-        return response
+            return name['familyName']
+        raise AttributeError("family_name not found")
 
     @property
     def name(self):
         return getattr(self, 'userName')
 
-    def load(self):
-        for prop in self.properties:
-            try:
-                value = getattr(self, prop)
-                self._prop_dict[prop] = value
-            except AttributeError as atbe:
-                pass
-
     def __getattr__(self, name):
-        if name in self._user_cf:
-            return self._user_cf[name]
+        if name in self._config:
+            return self._config[name]
         elif name in self._user_uaa:
             return self._user_uaa[name]
         raise AttributeError("%s not found" % name)
@@ -361,39 +319,3 @@ class Exporter:
             org_list.append(o.asdict())
         return org_list
 
-
-    def get_variable_group(self):
-        response = self.client.request("GET", "/v2/config/environment_variable_groups/running")
-        return response
-
-    def get_user_default_org(self, guid):
-        pass
-
-    def get_user_default_space(self, guid):
-        response = self.client.request("GET", "/v2/spaces")
-        pass
-
-    def get_users_list(self):
-        response = self.client.request("GET", "/v2/users")
-        resources = response[0]['resources']
-        user_ids = [user['metadata']['guid'] for user in resources]
-        return user_ids
-
-    def get_user_details(self, guid):
-        user = None
-        user_dict = collections.OrderedDict()
-        try:
-            user = self.client.uaa.user_get(user_id)
-            user_dict['name'] = user['userName']
-            user_dict['email'] = [email["value"] for email in user['emails'] if email['primary']]
-            user_dict['familyName'] = user['name']['familyName']
-            user_dict['givenName'] = user['name']['givenName']
-            user_dict['active'] = user['active']
-        except UAAException as uaaexp:
-            print(uaaexp)
-
-        return user_dict
-
-    def transform_in_valid_yaml(self, obj):
-        yaml_string = pyaml.dump(obj)
-        print(yaml_string)
