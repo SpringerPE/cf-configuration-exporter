eda1eb1bae892d8a32246b4e8e7cfbec42f343d1
Claudio Benfatto <claudio.benfatto@gmail.com>
2017-02-15 10:03:06 +0000

    Add tests

---
+++ test/exporter_tests.py
@@ -0,0 +1,14 @@
+import unittest
+import json
+import fixtures
+from exporter.exporter import SecurityGroup, SecurityRule
+
+class TestExporter(unittest.TestCase):
+
+	def test_security_rule(self):
+		sec_rule_obj = json.loads(fixtures.security_group_response)
+		sec_rule = SecurityRule(sec_rule_obj['entity']['rules'][0])
+		sec_rule.load()
+		self.assertEquals(sec_rule.sec_rule['protocol'], 'udp')
+		self.assertEquals(sec_rule.sec_rule['ports'], '8080')
+		self.assertEquals(sec_rule.sec_rule['destination'], '198.41.191.47/1')
