61fd20ecab15227140b73f8c2425fee6d927a642
Claudio Benfatto <claudio.benfatto@gmail.com>
2017-02-25 13:06:20 +0000

    use jinja2 template in generating the manifest instead of dumping yaml

--- exporter/run.py
+++ exporter/run.py
@@ -4,12 +4,17 @@ import pyaml
 import logging
 import sys
 
+from jinja2 import Environment, PackageLoader, select_autoescape
 from .exporter import Exporter
 from cfconfigurator.cf import CF
 from cfconfigurator.uaa import UAA, UAAException
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+env = Environment(
+    loader=PackageLoader('exporter', 'templates'),
+)
 
 api_url = os.environ.get("EXPORTER_API_URL", None)
 admin_user = os.environ.get("EXPORTER_ADMIN_USER", None)
@@ -36,6 +41,12 @@ def main():
 	exp = Exporter(cf_client)
 	exp.generate_manifest()
 
+	manifest = {key: pyaml.dump({key:value}) for key, value in exp.manifest.items()}
+
+	template = env.get_template('manifest.j2')
+	rendered = template.render(manifest=manifest)
+
+
 	with open(output_file ,"w") as stream:
-		pyaml.dump(exp.manifest, stream, explicit_start=True)
+		stream.write(rendered)
 
