043a409340cabee1c640bade88967b4d61fb26c1
Claudio Benfatto <claudio.benfatto@gmail.com>
2017-03-21 11:58:48 +0000

    bump version to 0.0.11

--- exporter/__init__.py
+++ exporter/__init__.py
@@ -5,7 +5,7 @@ cf-configuration-exporter
 """
 
 __program__ = "cf-configuration-exporter"
-__version__ = "0.0.10"
+__version__ = "0.0.11"
 __author__ = "Claudio Benfatto"
 __year__ = "2017"
 __email__ = "<claudio.benfatto@springer.com>"
